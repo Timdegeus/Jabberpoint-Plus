0a62dc459a61488b4b8662f73625d77c3aa46dde
Timdegeus <tim.de.geus@student.nhlstenden.com>
2024-04-03 12:15:39 +0200

    Added a unit test for testing purposes

---
+++ src/test/java/Controls/NextSlideCommandTest.java
@@ -0,0 +1,14 @@
+package Controls;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NextSlideCommandTest
+{
+
+    @Test
+    void execute()
+    {
+    }
+}
