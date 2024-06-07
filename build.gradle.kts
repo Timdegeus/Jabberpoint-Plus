47d07bc54691cbc61b26e1e553f04637c026f23f
Timdegeus <tim.de.geus@student.nhlstenden.com>
2024-06-07 19:05:52 +0200

    Removed sonarqube

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("java")
-    id("org.sonarqube") version "4.4.1.3373"
 }
 
 group = "org.example"
@@ -17,12 +16,4 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
-}
-
-sonar {
-    properties {
-        property("sonar.projectKey", "Timdegeus_Jabberpoint-Plus")
-        property("sonar.organization", "timdegeus")
-        property("sonar.host.url", "https://sonarcloud.io")
-    }
 }
